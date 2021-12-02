5bec798cc763549e39f713ba2ebf992ffa04baed
Nikolay <36446176+mail2nnv@users.noreply.github.com>
2021-12-02 12:43:53 +0300

    simplificate Provide() (#4)
    * simplificate Provide()
    
    * extract keyspace creation outside of the package

--- provide.go
+++ provide.go
@@ -4,7 +4,17 @@
 
 package istoragecas
 
+import (
+	istorage "github.com/heeus/core-istorage"
+	istructs "github.com/heeus/core-istructs"
+)
+
 // Provide s.e.
-func Provide() AppStorageProviderFuncType {
-	return implAppStorageProvider
+func Provide(casPar CassandraParamsType, apps map[istructs.AppName]AppCassandraParamsType) istorage.IAppStorageProvider {
+	provider, err := newStorageProvider(casPar, apps)
+	if err != nil {
+		panic(err)
+	}
+
+	return provider
 }
